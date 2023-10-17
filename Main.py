ffba43bbc17cf4b35851a152e0ed18db4302beb8
raul <raulandrei2017@yahoo.com>
2023-10-17 11:07:30 +0100

    some comments added

--- Main.py
+++ Main.py
@@ -6,8 +6,10 @@ subject2 = Subject("CA", "CS2014", 2, 5)
 subject3 = Subject("AA", "CS4018", 4, 5)
 subject4 = Subject("DB", "CS1016", 1, 5)
 
+#so here i pass a list of subjects to my student constructor
 raul = Student("Spatariu Raul", 20, "Fagaras", "raulandrei2019@gmail.com", "123124301",
                 "superSecretPassword", 2, [subject1, subject2, subject3])
 
+#here I want to be able to see how many credits I have
 print(raul.get_total_credits)
 
