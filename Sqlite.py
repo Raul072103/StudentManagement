3a6e131817acbef5ba92d5e4f58bf1c420f9a7cb
raul <raulandrei2017@yahoo.com>
2023-10-20 11:44:52 +0100

    some sqlite functions added

---
+++ Sqlite.py
@@ -0,0 +1,19 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_connection(r"C:\Users\raula\Desktop\facultate\anul 2 sem 1\Intermediate programming\Assignment\UniversitySystem\University.db")
+
+
